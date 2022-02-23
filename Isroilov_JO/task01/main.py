a045a979e1185735bdbc3f35bf6311616a04e6a3
Jasur <woot_1999@mail.ru>
2022-02-23 14:41:20 +0300

    The code is modified. All mistakes are corrected

--- Isroilov_JO/task01/main.py
+++ Isroilov_JO/task01/main.py
@@ -1,26 +1,37 @@
 import sys
 
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
 
 def main(args: list):
-    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    res = ''
+
+    res = list([])
+    status = 1
     s = args[1]
-    mod = args[2]
 
-    if args[0] == 'inc':
-        for sym in s:
-            if sym in alphabet:
-                a = alphabet.find(sym)
-                new_mod = (a + int(mod)) % len(alphabet)
-                res += alphabet[new_mod]
-    elif args[0] == 'dec':
-        for sym in s:
-            if sym in alphabet:
-                a = alphabet.find(sym)
-                new_mod = (a - int(mod)) % len(alphabet)
-                res += alphabet[new_mod]
+    if len(args[0]) > 1:
+        print("Incorrect status input")
+        sys.exit(-1)
+    elif not args[2].isnumeric():
+        print("Incorrect step input")
+        sys.exit(-1)
 
-    print(res)
+    if args[0] == 'd':
+        status = -1
+
+    mod = int(args[2]) * status
+
+    for sym in s:
+        if sym in alphabet:
+            tmp = alphabet.find(sym)
+            new_mod = (tmp + mod) % len(alphabet)
+            res.append(alphabet[new_mod])
+        else:
+            print("Prohibited message input")
+            sys.exit(-1)
+
+    msg = "".join(res)
+    print(msg)
 
 
 if __name__ == '__main__':
